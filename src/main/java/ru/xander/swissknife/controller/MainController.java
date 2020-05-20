8e4c047d634390d318c21c4b12d709e0cd241270
xander <xander.shahov@gmail.com>
2020-05-20 20:17:34 +0300

    add - добавлен таб для работы с генератором jaxb-кода

--- src/main/java/ru/xander/swissknife/controller/MainController.java
+++ src/main/java/ru/xander/swissknife/controller/MainController.java
@@ -2,9 +2,14 @@ package ru.xander.swissknife.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import lombok.Setter;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Component;
+import ru.xander.swissknife.controller.main.MainTabJaxb;
 
 /**
  * @author Alexander Shakhov
@@ -12,12 +17,26 @@ import org.springframework.stereotype.Component;
 @Component
 @FxmlView("/views/main.fxml")
 public class MainController {
-
     @Setter
-    private Scene scene;
+    public Scene scene;
+
+    @FXML
+    public Tab tabJaxb;
+    @FXML
+    public TextField textJaxbWsdlPath;
+    @FXML
+    public Button buttonChooseWsdlPath;
+    @FXML
+    public TextField textJaxbSrcPath;
+    @FXML
+    public Button buttonChooseSrcPath;
+    @FXML
+    public Button buttonJaxbGenerate;
+    @FXML
+    public TextArea textJaxbLog;
 
     @FXML
     public void initialize() {
-        // do nothing
+        MainTabJaxb.initialize(this);
     }
 }
---
+++ src/main/java/ru/xander/swissknife/controller/main/MainTabJaxb.java
@@ -0,0 +1,81 @@
+package ru.xander.swissknife.controller.main;
+
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import org.springframework.util.StringUtils;
+import ru.xander.swissknife.controller.MainController;
+
+import java.io.File;
+
+/**
+ * @author Alexander Shakhov
+ */
+@SuppressWarnings("WeakerAccess")
+public class MainTabJaxb {
+
+    private final MainController main;
+
+    private MainTabJaxb(MainController main) {
+        this.main = main;
+    }
+
+    private void initializeTab() {
+        main.buttonChooseWsdlPath.setOnAction(event -> chooseWsdlPath());
+        main.buttonChooseSrcPath.setOnAction(event -> chooseSourcePath());
+    }
+
+    public static void initialize(MainController main) {
+        new MainTabJaxb(main).initializeTab();
+    }
+
+    public void chooseWsdlPath() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("WSDL and XSD", "*.wsdl;*.xsd"),
+                new FileChooser.ExtensionFilter("WSDL", "*.wsdl"),
+                new FileChooser.ExtensionFilter("XSD", "*.xsd")
+        );
+        String currentWsdlFile = main.textJaxbWsdlPath.getText();
+        String currentSourcePath = main.textJaxbSrcPath.getText();
+        if (!StringUtils.isEmpty(currentWsdlFile)) {
+            File initialFile = new File(currentWsdlFile);
+            if (initialFile.getParentFile().exists()) {
+                fileChooser.setInitialDirectory(initialFile.getParentFile());
+            }
+        } else if (!StringUtils.isEmpty(currentSourcePath)) {
+            File initialDirectory = new File(currentSourcePath);
+            if (initialDirectory.exists()) {
+                fileChooser.setInitialDirectory(initialDirectory);
+            }
+        } else {
+            fileChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
+        }
+        File wsdlFile = fileChooser.showOpenDialog(main.scene.getWindow());
+        if (wsdlFile != null) {
+            main.textJaxbWsdlPath.setText(wsdlFile.getAbsolutePath());
+        }
+    }
+
+    public void chooseSourcePath() {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        String currentWsdlFile = main.textJaxbWsdlPath.getText();
+        String currentSourcePath = main.textJaxbSrcPath.getText();
+        if (!StringUtils.isEmpty(currentSourcePath)) {
+            File initialDirectory = new File(currentSourcePath);
+            if (initialDirectory.exists()) {
+                directoryChooser.setInitialDirectory(initialDirectory);
+            }
+        } else if (!StringUtils.isEmpty(currentWsdlFile)) {
+            File initialFile = new File(currentWsdlFile);
+            if (initialFile.getParentFile().exists()) {
+                directoryChooser.setInitialDirectory(initialFile.getParentFile());
+            }
+        } else {
+            directoryChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
+        }
+        File sourcePath = directoryChooser.showDialog(main.scene.getWindow());
+        if (sourcePath != null) {
+            main.textJaxbSrcPath.setText(sourcePath.getAbsolutePath());
+        }
+    }
+}
